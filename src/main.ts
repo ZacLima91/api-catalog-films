3f63a2fa022563c4f1aa117bd6d140d735601d7e
zac <jadielsonmanoel@gmail.com>
2022-09-21 18:35:48 -0300

    feature: configured the port for connection to heroku

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 8080;
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -17,6 +18,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  await app.listen(port);
 }
 bootstrap();
