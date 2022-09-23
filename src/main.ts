cfdaf6a14642f1d8e5af7a0e8bd070521ab547c7
zac <jadielsonmanoel@gmail.com>
2022-09-22 23:00:21 -0300

    feature: configured o cors

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors);
   app.useGlobalPipes(new ValidationPipe());
 
-    const config = new DocumentBuilder()
-      .setTitle('Cine Pipoca')
-      .setDescription('Api responsável pela gestão do Cine Pipoca')
-      .setVersion('1.0')
-      .addTag('films')
-      .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('Cine Pipoca')
+    .setDescription('Api responsável pela gestão do Cine Pipoca')
+    .setVersion('1.0')
+    .addTag('films')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 8080);
 }
