0869ad1203a9ab6db2a17c4e1fc6cebc15ce1add
zac <jadielsonmanoel@gmail.com>
2022-09-22 23:48:59 -0300

    feature: configured o cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors);
+  app.use(cors());
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
